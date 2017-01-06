017970c5ce2640f973681b7c1a4f10cffba2925e
dramaix <dramaix@google.com>
2017-01-05 16:46:08 -0800

    - Delete jsinterop.base/ArrayLike/JsArray/Object - Add support for conversion of IArrayLike and IObject
    PiperOrigin-RevId: 143725259

--- java/jsinterop/base/ArrayLike.java
+++
@@ -1,9 +0,0 @@
-package jsinterop.base;
-
-import jsinterop.annotations.JsPackage;
-import jsinterop.annotations.JsType;
-
-// TODO(b/33437732): remove
-/** Place holder fot ArrayLike interface. Complete implementation will be provided later. */
-@JsType(isNative = true, name = "IArrayLike", namespace = JsPackage.GLOBAL)
-public interface ArrayLike<T> extends JsArrayLike<T> {}
