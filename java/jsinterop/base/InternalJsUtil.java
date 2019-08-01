fad429fb7cefc63221b4f214aa3b641499faf2c7
Julien Dramaix <dramaix@google.com>
2019-08-01 10:11:12 -0700

    Use // J2CL_ONLY (with a space) everywhere and remove the regex in sed command
    \? is a GNU extension and work only with GNU sed. macOS doesn't have GNU sed installed by default and the build failed on macOs.
    
    PiperOrigin-RevId: 261144387

--- java/jsinterop/base/InternalJsUtil.java
+++ java/jsinterop/base/InternalJsUtil.java
@@ -18,18 +18,18 @@ package jsinterop.base;
 
 import com.google.gwt.core.client.UnsafeNativeLong;
 import javaemul.internal.annotations.HasNoSideEffects;
-//J2CL_ONLY import jsinterop.annotations.JsMethod;
-//J2CL_ONLY import jsinterop.annotations.JsPackage;
+// J2CL_ONLY import jsinterop.annotations.JsMethod;
+// J2CL_ONLY import jsinterop.annotations.JsPackage;
 
 /** Utilities used internally to interact with native code. */
 class InternalJsUtil {
 
-  //J2CL_ONLY @JsMethod
+  // J2CL_ONLY @JsMethod
   public static native JsPropertyMap<Object> emptyObjectLiteral() /*-{
     return {};
   }-*/;
 
-  //J2CL_ONLY @JsMethod(namespace = JsPackage.GLOBAL, name = "goog.getObjectByName")
+  // J2CL_ONLY @JsMethod(namespace = JsPackage.GLOBAL, name = "goog.getObjectByName")
   public static native Object getObjectByName(String qualifiedName, Object obj) /*-{
     var parts = qualifiedName.split('.');
     var cur = obj;
@@ -42,90 +42,90 @@ class InternalJsUtil {
     return cur;
   }-*/;
 
-  //J2CL_ONLY @JsMethod(name="getIndexed")
+  // J2CL_ONLY @JsMethod(name="getIndexed")
   public static native Object get(Object obj, String key) /*-{
     return obj[key];
   }-*/;
 
-  //J2CL_ONLY @JsMethod(name="hasIndexed")
+  // J2CL_ONLY @JsMethod(name="hasIndexed")
   public static native boolean has(Object obj, String key) /*-{
     return key in obj;
   }-*/;
 
-  //J2CL_ONLY @JsMethod(name="deleteIndexed")
+  // J2CL_ONLY @JsMethod(name="deleteIndexed")
   public static native Object delete(Object obj, String key) /*-{
     delete obj[key];
   }-*/;
 
-  //J2CL_ONLY @JsMethod(name="setIndexed")
+  // J2CL_ONLY @JsMethod(name="setIndexed")
   public static native void set(Object obj, String key, Object value) /*-{
     obj[key] = value;
   }-*/;
 
-  //J2CL_ONLY @JsMethod(name="getIndexed")
+  // J2CL_ONLY @JsMethod(name="getIndexed")
   public static native Object getAt(Object obj, int key) /*-{
     return obj[key];
   }-*/;
 
-  //J2CL_ONLY @JsMethod(name="hasIndexed")
+  // J2CL_ONLY @JsMethod(name="hasIndexed")
   public static native boolean hasAt(Object obj, int key) /*-{
     return key in obj;
   }-*/;
 
-  //J2CL_ONLY @JsMethod(name="deleteIndexed")
+  // J2CL_ONLY @JsMethod(name="deleteIndexed")
   public static native void deleteAt(Object obj, int key) /*-{
     delete obj[key];
   }-*/;
 
-  //J2CL_ONLY @JsMethod(name="setIndexed")
+  // J2CL_ONLY @JsMethod(name="setIndexed")
   public static native void setAt(Object obj, int key, Object value) /*-{
     obj[key] = value;
   }-*/;
 
-  //J2CL_ONLY @JsMethod
+  // J2CL_ONLY @JsMethod
   public static native void forEach(Object obj, JsForEachCallbackFn cb) /*-{
     for (var element in obj) {
       cb(element);
     }
   }-*/;
 
-  //J2CL_ONLY @JsMethod(name="castToAny")
+  // J2CL_ONLY @JsMethod(name="castToAny")
   public static native boolean asBoolean(Object obj) /*-{
    return obj;
   }-*/;
 
-  //J2CL_ONLY @JsMethod(name="castToAny")
+  // J2CL_ONLY @JsMethod(name="castToAny")
   public static native double asDouble(Object obj) /*-{
    return obj;
   }-*/;
 
-  //J2CL_ONLY @JsMethod(name="castToAny")
+  // J2CL_ONLY @JsMethod(name="castToAny")
   public static native int asInt(Object obj) /*-{
     return obj;
   }-*/;
 
-  //J2CL_ONLY @JsMethod(name="castToAny")
+  // J2CL_ONLY @JsMethod(name="castToAny")
   public static native short asShort(Object obj) /*-{
     return obj;
   }-*/;
 
-  //J2CL_ONLY @JsMethod(name="castToAny")
+  // J2CL_ONLY @JsMethod(name="castToAny")
   public static native char asChar(Object obj) /*-{
     return obj;
   }-*/;
 
-  //J2CL_ONLY @JsMethod(name="castToAny")
+  // J2CL_ONLY @JsMethod(name="castToAny")
   public static native byte asByte(Object obj) /*-{
     return obj;
   }-*/;
 
   @UnsafeNativeLong
-  //J2CL_ONLY @JsMethod(name="castToAny")
+  // J2CL_ONLY @JsMethod(name="castToAny")
   public static native long asLong(Object obj) /*-{
     return obj;
   }-*/;
 
-  //J2CL_ONLY @JsMethod
+  // J2CL_ONLY @JsMethod
   @HasNoSideEffects
   public static native boolean isLong(Object obj) /*-{
     if (typeof obj == 'number') {
@@ -137,40 +137,40 @@ class InternalJsUtil {
     return false;
   }-*/;
 
-  //J2CL_ONLY @JsMethod
+  // J2CL_ONLY @JsMethod
   @HasNoSideEffects
   public static native boolean isInt(Object obj) /*-{
     return (obj|0) === obj;
   }-*/;
 
-  //J2CL_ONLY @JsMethod
+  // J2CL_ONLY @JsMethod
   @HasNoSideEffects
   public static native boolean hasLength(Object obj) /*-{
     return typeof obj == 'object' && typeof obj.length == 'number';
   }-*/;
 
-  //J2CL_ONLY @JsMethod
+  // J2CL_ONLY @JsMethod
   @HasNoSideEffects
   public static native int getLength(JsArrayLike<?> obj) /*-{
     return obj.length;
   }-*/;
 
-  //J2CL_ONLY @JsMethod
+  // J2CL_ONLY @JsMethod
   public static native void setLength(JsArrayLike<?> obj, int length) /*-{
     return obj.length = length;
   }-*/;
 
-  //J2CL_ONLY @JsMethod
+  // J2CL_ONLY @JsMethod
   public static native <T> JsConstructorFn<T> toCtor(Class<T> clazz) /*-{
     return clazz.@java.lang.Class::jsConstructor;
   }-*/;
 
-  //J2CL_ONLY @JsMethod(namespace="java.lang.Class", name="$get")
+  // J2CL_ONLY @JsMethod(namespace="java.lang.Class", name="$get")
   public static native <T> Class<T> toClass(JsConstructorFn<T> ctor) /*-{
     return ctor.prototype.@java.lang.Object::___clazz;
   }-*/;
 
-  //J2CL_ONLY @JsMethod(namespace=jsinterop.annotations.JsPackage.GLOBAL, name="Reflect.construct")
+  // J2CL_ONLY @JsMethod(namespace=jsinterop.annotations.JsPackage.GLOBAL, name="Reflect.construct")
   public static native <T> T construct(JsConstructorFn<T> ctor, Object[] args) /*-{
     return new (ctor.bind.apply(ctor, [null].concat(args)));
   }-*/;
